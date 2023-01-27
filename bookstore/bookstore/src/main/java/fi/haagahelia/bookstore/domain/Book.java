b97220e6c689af9e95a41143e6d697fe2b2a96e8
annimank <118170212+annimank@users.noreply.github.com>
2023-01-27 09:45:48 +0200

    Add files via upload

---
+++ bookstore/bookstore/src/main/java/fi/haagahelia/bookstore/domain/Book.java
@@ -0,0 +1,43 @@
+package fi.haagahelia.bookstore.domain;
+
+public class Book {
+	
+	private String isbn;
+	private String title, author;
+	private int pubYear;
+	private float price;
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPubYear() {
+		return pubYear;
+	}
+	public void setPubYear(int pubYear) {
+		this.pubYear = pubYear;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+	
+}
---
+++ bookstore/bookstore/src/test/java/fi/haagahelia/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

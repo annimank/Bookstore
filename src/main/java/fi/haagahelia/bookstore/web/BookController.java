b6270af76fe3e9f853a0b8aae1629652961f7561
annimank <mankki.anni@gmail.com>
2023-02-01 13:49:00 +0200

    Ch3 Bookstore Listpage

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,18 +1,28 @@
 package fi.haagahelia.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
 	
 	@RequestMapping("/index")
 	public String Bookstore() {
 		return "bookstore";
 	}
 	
-	
+	@GetMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
 
 }

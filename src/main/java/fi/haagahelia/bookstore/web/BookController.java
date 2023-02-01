9b1a5d8b9e6e2ac7076e69eebcc557c0d747ea9e
annimank <mankki.anni@gmail.com>
2023-02-01 14:12:37 +0200

    Ch3 Bookstore CRUD

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
@@ -24,5 +27,23 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value = "/delete/{isbn}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("isbn") String isbn, Model model) {
+    	repository.deleteById(isbn);
+        return "redirect:../booklist";
+    }     
+	
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }   
 
 }
